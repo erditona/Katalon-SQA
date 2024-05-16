7bd0e6d071c0a5663defd14b1f5f56759bd154aa
erditona <erditonaushaadam@gmail.com>
2024-05-16 17:11:04 +0700

    add test suites UserAutority dan report

---
+++ Test Suites/UserAutority Test Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>SignUp Test Suite</description>
+   <name>UserAutority Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>206a7573-c91b-4a81-bf52-0d5e4617cffc</testSuiteGuid>
+   <testCaseLink>
+      <guid>a7a700d7-9934-4b11-9d9d-49a1b4ebb7f0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SignUp</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
